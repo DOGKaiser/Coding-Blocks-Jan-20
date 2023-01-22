08a78c951192dcd26c723eb65558a03ba604b48d
DOGKaiser <mattknizackyjr@gmail.com>
2023-01-22 15:32:21 -0500

    Skip logo menu, fix for settings menu

--- Assets/_Game/Game/Menus/Logo/LogoMenuCustom.cs
+++ Assets/_Game/Game/Menus/Logo/LogoMenuCustom.cs
@@ -3,5 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class LogoMenuCustom : LogoMenu {
-	
+	void Start() {
+		Next();
+		Close();
+	}
+
+	protected virtual void Next() {
+		if(skipLogin) {
+			MainMenu.TransitionIn();
+		}
+		else {
+			Tools.CallStaticFunctionOfClass(LoginAssembly, LoginClass, LoginMethod);
+		}
+	}
 }
