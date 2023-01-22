7b2370aeebda3847faa2e34cbd231d7ddd2278f1
imacquarrie <iainc.macquarrie@gmail.com>
2023-01-21 20:53:32 -0500

    Added/Implemented Grip Meter UI Added sprites for background and water effects

--- Assets/_Game/Game/Menus/MatchMenu/MatchMenuCustom.cs
+++ Assets/_Game/Game/Menus/MatchMenu/MatchMenuCustom.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.UI;
 
 public class MatchMenuCustom : MatchMenu {
 
@@ -9,7 +10,7 @@ public class MatchMenuCustom : MatchMenu {
     [SerializeField] private GameObject PlayerPrefab;
     [HideInInspector] public GameObject Player;
     
-    [SerializeField] private TextMeshProUGUI GripMeter;
+    [SerializeField] private Image GripMeter;
 
     public override void StartMatchFromServer(int seed) {
 
@@ -21,10 +22,11 @@ public class MatchMenuCustom : MatchMenu {
 
         ((MatchGameCustom)CurrentMatch).fisher = MatchArea.transform.Find("Fisher").GetComponent<Fisher>();
         Player = ObjectPoolMgr.Instance.GetObject(PlayerPrefab, MatchArea.transform);
-        SetGrip(0);
+        SetGrip(100);
     }
 
     public void SetGrip(float grip) {
-        GripMeter.text = ((int)grip).ToString();
+        Debug.Log(grip+" "+ ((int)grip));
+        GripMeter.rectTransform.localScale = new Vector3(1,(grip/100),1);
     }
 }
